1c4c635e6557c25f3ad2c93a485c5d64ee6aead9
Lastor <lastoraya@gmail.com>
2020-02-24 10:53:34 +0800

    [加入] 組件 Modal

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div id="app">
+    <!-- Navbar -->
     <Navbar/>
+
+    <!-- mode 切換 & 搜尋提示 -->
     <ModeBar/>
+
+    <!-- main content -->
     <main role="main" class="pt-3">
       <router-view/>
     </main>
+
+    <!-- Bootstrap Modal -->
+    <Modal/>
   </div>
 </template>
 
@@ -13,12 +21,14 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar.vue'
 import ModeBar from './components/ModeBar.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
   components: {
     Navbar,
-    ModeBar
+    ModeBar,
+    Modal
   }
 }
 </script>
