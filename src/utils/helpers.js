3dec2b6058ab6009135592bf58395044d2617fe2
Lastor <lastoraya@gmail.com>
2020-02-25 13:25:44 +0800

    [引入] sweetalert2 package，建立 helpers

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,16 @@
 import axios from 'axios'
+import Swal from 'sweetalert2'
 
 const apiHelper = axios.create({
   baseURL: 'https://lighthouse-user-api.herokuapp.com/api/v1'
 })
 
-export { apiHelper }
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true
+})
+
+export { apiHelper, Toast }
